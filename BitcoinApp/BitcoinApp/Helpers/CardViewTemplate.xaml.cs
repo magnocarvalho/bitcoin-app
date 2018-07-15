c63621bf82604d0b1c7c3ea6101bfb7a79b2490b
lucasfcoelho1 <lucasfcoelho1@gmail.com>
2018-07-15 12:12:55 -0300

    Desenho da interface com o gráfico

--- BitcoinApp/BitcoinApp/Helpers/CardViewTemplate.xaml.cs
+++ BitcoinApp/BitcoinApp/Helpers/CardViewTemplate.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microcharts.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microcharts;
+using SkiaSharp;
+using Entry = Microcharts.Entry;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,10 +16,16 @@ namespace BitcoinApp.Helpers
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CardViewTemplate : ContentView
 	{
-		public CardViewTemplate ()
+        List<Entry> entries = new List<Entry>
+        {
+            new Entry(200) {Color = SKColor.Parse("#003E06"), Label = "Janeiro", ValueLabel = "200"},
+            new Entry(400) {Color = SKColor.Parse("#003E06"), Label = "Fevereiro", ValueLabel = "400"},
+            new Entry(-100) {Color = SKColor.Parse("#003E06"), Label = "Março", ValueLabel = "-100"}
+        };
+        public CardViewTemplate ()
 		{
 			InitializeComponent ();
-            
-		}
+            chartView.Chart = new LineChart { Entries = entries };
+        }
 	}
 }
--- BitcoinApp/BitcoinApp/Views/MainPage.xaml.cs
+++ BitcoinApp/BitcoinApp/Views/MainPage.xaml.cs
@@ -1,21 +1,37 @@
 ﻿using BitcoinApp.ViewModel;
+using Microcharts;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Entry = Microcharts.Entry;
 
 namespace BitcoinApp
 {
 	public partial class MainPage : ContentPage
 	{
         public MainVieWModel ViewModel { get; set; }
+        List<Entry> entries = new List<Entry>
+        {
+            new Entry(6.764f) {Color = SKColor.Parse("#003E06"), Label = "Segunda", ValueLabel = "6.764"},
+            new Entry(6.780f) {Color = SKColor.Parse("#003E06"), Label = "Terça", ValueLabel = "6.780"},
+            new Entry(3.768f) {Color = SKColor.Parse("#003E06"), Label = "Quarta", ValueLabel = "3.768"},
+            new Entry(6.736f) {Color = SKColor.Parse("#003E06"), Label = "Quinta", ValueLabel = "6.736"},
+            new Entry(6.688f) {Color = SKColor.Parse("#003E06"), Label = "Sexta", ValueLabel = "6.688"},
+            new Entry(6.638f) {Color = SKColor.Parse("#003E06"), Label = "Sábado", ValueLabel = "6.638"},
+            new Entry(6.564f) {Color = SKColor.Parse("#003E06"), Label = "Domingo", ValueLabel = "6.564"}
+        };
         public MainPage()
 		{
 			InitializeComponent();
             ViewModel = new MainVieWModel();
             BindingContext = ViewModel;
-		}
+            chartView.Chart = new LineChart { Entries = entries };
+            chartView.Chart.LabelTextSize = 25;
+
+        }
 	}
 }
