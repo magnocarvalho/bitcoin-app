e02474d5f012380d3a7c7a561e732e6d1c2803b2
lucasfcoelho1 <lucasfcoelho1@gmail.com>
2018-07-15 12:31:30 -0300

    Removido o CarDataModel. Adicionada a classe MarketPrice

---
+++ BitcoinApp/BitcoinApp/Model/MarketPrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoinApp.Model
+{
+    public class MarketPrice
+    {
+        public string Status { get; set; }
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public string Period { get; set; }
+        public string Description { get; set; }
+        public List<Value> Values { get; set; }
+    }
+}
---
+++ BitcoinApp/BitcoinApp/Model/Value.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoinApp.Model
+{
+    public class Value
+    {
+        [JsonProperty("x")]
+        public int TimeStamp { get; set; }
+        [JsonProperty("y")]
+        public double UsdPrice { get; set; }
+    }
+}
